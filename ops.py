b43c5a243ae8bd8a5142aa54d7267487129f2d5a
shivapaladugula <81111233+shivapaladugula@users.noreply.github.com>
2022-10-11 15:20:06 +0530

    Update ops.py

--- ops.py
+++ ops.py
@@ -1,17 +1,5 @@
-# Python Program to find the area of triangle when all three side-lengths are known!
+# for item in # this python program asks uses for 2 number and returns their sum:
 
-a = 5
-b = 6
-c = 7
-
-# Uncomment below to take inputs from the user
-# a = float(input('Enter first side: '))
-# b = float(input('Enter second side: '))
-# c = float(input('Enter third side: '))
-
-# calculate the semi-perimeter
-s = (a + b + c) / 2
-
-# calculate the area
-area = (s*(s-a)*(s-b)*(s-c)) ** 0.5
-print('The area of the triangle is: ' + area)
+a = int(input("enter first Number: "))
+b = int(input("enter second Number: "))
+print("sum is:", a+b)
