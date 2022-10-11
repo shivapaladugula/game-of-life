27126abf5d29395f0c093ede4e9af6ed8e673d1a
shivapaladugula <81111233+shivapaladugula@users.noreply.github.com>
2022-10-11 15:21:44 +0530

    Update ops.py

--- ops.py
+++ ops.py
@@ -1,5 +1,1 @@
-# for item in # this python program asks uses for 2 number and returns their sum:
-
-a = int(input("enter first Number: "))
-b = int(input("enter second Number: "))
-print("sum is:", a+b)
+print('Hello, world!')
